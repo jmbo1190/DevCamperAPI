be73feb37994457539a5ab43d8d21c1d4101b3fa
Jean-Michel Bodart <jmbodart@skynet.be>
2023-01-17 22:14:42 +0100

    Update server.js
    Create dummy basic routes

--- server.js
+++ server.js
@@ -14,6 +14,26 @@ app.get("/", (req, res) => {
     res.status(200).json( { success: true, data: { id: 1 } } );
 });
 
+app.get("/api/v1/bootcamps", (req, res) => {
+    res.status(200).json( { success: true, msg: 'Return all bootcamps' } ); // later on we will return data from database
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+    res.status(200).json( { success: true, msg: `Return bootcamp ${req.params.id}` } ); 
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+    res.status(200).json( { success: true, msg: 'Create new bootcamp' } ); 
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+    res.status(200).json( { success: true, msg: `Update bootcamp ${req.params.id}` } ); 
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+    res.status(200).json( { success: true, msg: `Delete bootcamp ${req.params.id}` } ); 
+});
+
 const PORT=process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Express server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`));
