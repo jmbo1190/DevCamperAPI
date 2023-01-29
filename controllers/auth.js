262eee79ece89b736dff52eb652049289fd787a7
Jean-Michel Bodart <jmbodart@skynet.be>
2023-01-29 17:51:18 +0100

    Update controllers/auth.js
    Implement adding user to DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,19 @@ const User = require('../models/User');
 // @access  Public
 exports.register = asyncHandler(
     async (req, res, next) => {
-        
+        const { name, email, password, role } = req.body; // pull data from the request body
+
+        // Create user
+        const user = await User.create({
+            name,
+            email,
+            password,   // password hashing implemented in separate middleware
+            role
+        });
+
+        // Ultimately we will send back a token
+        // but right now we just want the user registered
+
         res.status(200).json({ success: true });  // initial simple method
     }
 );
