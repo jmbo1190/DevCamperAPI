d4cc38684a4c0ef398f285a97aff6aa3a02c517e
Jean-Michel Bodart <jmbodart@skynet.be>
2023-01-31 22:19:34 +0100

    Update middleware/async.js
    Fix asyncHandler function

--- middleware/async.js
+++ middleware/async.js
@@ -1,8 +1,13 @@
 //const asyncHandler = fn => (req, res, next) =>
 //    Promise.resolve(fn(req, res, next)).catch(next);
 
+/*
 const asyncHandler = (fn) => {
     return (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
 }
+*/
+
+const asyncHandler = fn => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
 
 module.exports = asyncHandler;
