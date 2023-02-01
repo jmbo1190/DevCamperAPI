10e57b69db2202d66ebc985cc97c44428d191404
Jean-Michel Bodart <jmbodart@skynet.be>
2023-02-01 06:25:28 +0100

    Update controllers/auth.js
    Add functiion getMe() to get the logged in user

--- controllers/auth.js
+++ controllers/auth.js
@@ -103,3 +103,19 @@ const sendTokenResponse = (user, statusCode, res) => {
         .cookie('token', token, options)
         .json({ success: true, token });
 }
+
+
+
+// @desc    Get the current logged in user
+// @route   POST /api/v1/auth/me
+// @access  Private (i.e. you need a token to access)
+exports.getMe = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json(
+        {
+            success: true,
+            data: user
+        }
+    )
+});
