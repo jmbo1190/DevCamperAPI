4c3d12a0373ba1de79cfa978f5e5500df6b6f03a
Jean-Michel Bodart <jmbodart@skynet.be>
2023-02-01 12:36:52 +0100

    Update seeder.js
    Add  import and delete of users

--- seeder.js
+++ seeder.js
@@ -10,6 +10,7 @@ dotenv.config( {path: "./config/config.env"} );
 
 // Load Models
 const Bootcamp = require("./models/Bootcamp");
+const User = require('./models/User');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -19,11 +20,13 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 const bootcamps = JSON.parse( fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8') );
+const users = JSON.parse( fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8') );
 
 // Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await User.create(users);
 
         console.log("Data imported.".green.inverse);
         process.exit();
@@ -38,7 +41,8 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
-
+        await User.deleteMany();
+        
         console.log("Data destroyed.".red.inverse);
         process.exit();
     } catch (err) {
