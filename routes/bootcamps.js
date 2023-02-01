2ab10269ea9d9bde49bef1a0341406838a590c11
Jean-Michel Bodart <jmbodart@skynet.be>
2023-02-01 08:02:40 +0100

    Update routes/bootcamps.js
    Use authorize() to let only publisher and admin roles create/update/delete bootcamps

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,29 +9,17 @@ const {
 
 const router = express.Router();
 
-const { protect } = require("../middleware/auth");
+const { protect, authorize } = require("../middleware/auth");
 
-router.route("/").get(getBootcamps).post(protect, createBootcamp);
-//router.get("/", (req, res) => {
-// res.status(200).json( { success: true, msg: 'Return all bootcamps' } ); // later on we will return data from database
-//});
-//router.post("/", (req, res) => {
-// res.status(200).json( { success: true, msg: 'Create new bootcamp' } );
-//});
+router
+  .route("/")
+  .get(getBootcamps)
+  .post(protect, authorize("publisher", "admin"), createBootcamp);
 
 router
   .route("/:id")
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp);
-// router.get("/:id", (req, res) => {
-// res.status(200).json( { success: true, msg: `Return bootcamp ${req.params.id}` } );
-// });
-// router.put("/:id", (req, res) => {
-// res.status(200).json( { success: true, msg: `Update bootcamp ${req.params.id}` } );
-// });
-// router.delete("/:id", (req, res) => {
-// res.status(200).json( { success: true, msg: `Delete bootcamp ${req.params.id}` } );
-// });
+  .put(protect, authorize("publisher", "admin"), updateBootcamp)
+  .delete(protect, authorize("publisher", "admin"), deleteBootcamp);
 
 module.exports = router;
