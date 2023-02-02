fd47289e159970ae4c62cd1a6af4021e6a2a9670
Jean-Michel Bodart <jmbodart@skynet.be>
2023-02-02 07:08:15 +0100

    Update routes/auth.js
    Create route auth/updatepassword

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
-const { protect } = require('../middleware/auth');
+const express = require("express");
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+} = require("../controllers/auth");
+const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.get('/me', protect, getMe);
-router.post('/forgotpassword', forgotPassword);
-router.put('/resetpassword/:resettoken', resetPassword);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, getMe);
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resettoken", resetPassword);
+router.put("/updatepassword", protect, updatePassword);
 
-module.exports = router ;
+module.exports = router;
