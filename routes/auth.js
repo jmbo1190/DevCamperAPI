53fd2fac4d0757b76fa4cd6038ca905919df1a98
Jean-Michel Bodart <jmbodart@skynet.be>
2023-02-04 10:41:34 +0100

    Update routes/auth.js
    Add route /auth/logout

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   register,
   login,
+  logout,
   getMe,
   forgotPassword,
   resetPassword,
@@ -14,6 +15,7 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.get("/logout", protect, logout);
 router.post("/forgotpassword", forgotPassword);
 router.put("/resetpassword/:resettoken", resetPassword);
 router.put("/updatepassword", protect, updatePassword);
