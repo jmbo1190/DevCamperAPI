459bd2aaa064a58f693b96d949796ab39ecde09b
Jean-Michel Bodart <jmbodart@skynet.be>
2023-02-11 20:47:36 +0100

    Update controllers/bootcamps.js
    Add function bootcampPhotoUpload()

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,7 +3,7 @@
 const Bootcamp = require("../models/Bootcamp");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
-
+const path = require('path');
 
 
 // @desc    Get all bootcamps
@@ -128,4 +128,76 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 
+
+// @desc    Upload photo for bootcamp
+// @route   PUT /api/v1/bootcamps/:id/photo
+// @access  Private
+
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+  let bootcamp = await Bootcamp.findById(req.params.id);
+  if (!bootcamp)
+    return next(
+      new ErrorResponse(`No bootcamp found with id of ${req.params.id}`, 404)
+    );
+
+
+  // Make sure user is the bootcamp owner or an admin
+  if (req.user.id !== bootcamp.user.toString() && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User with ID: ${req.user.id} is not authorized to update bootcamp with ID: ${req.params.id}`,
+        401
+      )
+    );
+  }
+
+  // Make sure a file was provided
+  if (! req.files) {
+    return next(
+      new ErrorResponse(`Please upload a file`, 400)
+    );
+  }
+
+  const file = req.files.file;
+  console.log(file);
+
+  // Make sure the file is an image
+  if (! file.mimetype.startsWith('image/')) {
+    return next(
+      new ErrorResponse(`Please upload an image file`, 400)
+    );
+  }
+
+// Check file size
+if (file.size > process.env.MAX_UPLOAD_FILE_SIZE) {
+  return next(
+    new ErrorResponse(`Please upload a file no bigger than ${process.env.MAX_UPLOAD_FILE_SIZE} bytes`, 400)
+  );
+}
+
+  // Create custom filename
+  file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
+  console.log('file.name: ', file.name);
+
+  // Move file to destination folder
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, 
+          async err => {
+            if (err) {
+              console.error(err);
+              return next(
+                new ErrorResponse(`Could not save uploaded file`, 500)
+              );
+            }
+
+            bootcamp = await Bootcamp.findByIdAndUpdate(bootcamp._id, { photo: file.name })
+
+            // EITHER return just the filename:
+            // res.status(200).json({ success: true, data: file.name });
+            // OR return the full bootcamp:
+            res.status(200).json({ success: true, data: bootcamp });
+            
+          });
+
+  
+});
 
